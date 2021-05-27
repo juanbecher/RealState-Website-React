5723a122a810326120fa2fe7c4d60e3162cd9e93
juanbecher <31422698+juanbecher@users.noreply.github.com>
2021-05-27 14:30:56 -0300

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyACAFDxDWSV3TAGIRrF7PswkGDduhaLCvI",
-    authDomain: "me-inmobiliaria.firebaseapp.com",
-    projectId: "me-inmobiliaria",
-    storageBucket: "me-inmobiliaria.appspot.com",
-    messagingSenderId: "386103398359",
-    appId: "1:386103398359:web:8772346abe83d9247f0592",
-    measurementId: "G-FP8N25M77Y"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
   export default firebaseConfig;
